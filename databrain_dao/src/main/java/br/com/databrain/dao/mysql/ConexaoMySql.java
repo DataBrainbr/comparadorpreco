f5219bbfd0aa3bf275f6949062e998aa13ae1884
GustavoGuersoni <gustavo.rios@databrain.com>
2014-03-08 16:23:23 -0300

    Novo projeto maven

---
+++ databrain_dao/src/main/java/br/com/databrain/dao/mysql/ConexaoMySql.java
@@ -0,0 +1,69 @@
+package br.com.databrain.dao.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class ConexaoMySql {
+
+    Connection conbco;
+    
+        public Connection abrirConexao() {
+
+        //atributo do tipo Connection 
+        //Connection connection = null;
+        try {
+            // Carregando o JDBC Driver padrão 
+            String driver = "com.mysql.jdbc.Driver";  //classe driver JDBC
+            String banco = "db_sistema";  //nome do banco
+            String host = "localhost";  //maquina onde está o banco
+            String str_conn = "jdbc:mysql://" + host + ":3307/" + banco; //URL de conexao
+            String usuario = "user_sistema";
+            String senha = "!@#$data%¨&*brain";
+            
+            Class.forName(driver); //carrega driver
+            
+            conbco = DriverManager.getConnection(str_conn, usuario, senha);
+            Statement stmt = conbco.createStatement();//cria stament para mandar um sql pra o banco
+            
+            System.out.println("conectou!");
+        } catch (ClassNotFoundException e) {
+            //Driver não encontrado 
+            System.out.println("O driver especificado nao foi encontrado.");
+            return null;
+        } 
+        catch (SQLException e) {
+            //Não conseguindo se conectar ao banco 
+            System.out.println("Nao foi possivel conectar ao Banco de Dados.");
+
+            return null;
+        }
+        return null;
+    }
+
+    public void executarComando(String comandoSQL) throws Exception {
+        Connection con = null;
+
+        try {
+            abrirConexao();
+        } catch (Exception ex) {
+            throw ex;
+        }
+        try {
+            String conexao = new String();
+            Statement comando = (Statement)conbco.createStatement();
+            // Insere retornando o ID criado.
+            comando.executeUpdate(comandoSQL);
+
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            //fecharDB();
+        }
+    }
+}
---
+++ robocapturatag_app/robocapturatag/src/main/java/br/com/databrain/robocapturatag/TesteTela.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.databrain.robocapturatag;
+
+import br.com.databrain.dao.mysql.ConexaoMySql;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class TesteTela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //ConexaoMySql conexao = new ConexaoMySql();
+        String strTexto = new String();
+        String strOpt = new String();
+        strOpt = JOptionPane.showInputDialog(null,"Deseja incluir ou excluir? (I/E) ");
+        if ("I".equals(strOpt.toUpperCase())) {
+            strTexto = JOptionPane.showInputDialog(null,"Entre com o valor para cadastro: ");
+            try {
+                salvarTeste(strTexto);
+            } catch (Exception ex) {
+                System.out.println("Erro ao gravar registro");
+            }            
+        } else {
+            strTexto = JOptionPane.showInputDialog(null,"Entre com o valor para excluir: ");
+            
+            try {
+                excluirTeste(strTexto);
+            } catch (Exception ex) {
+                System.out.println("Erro ao excluir registro");
+            }            
+        }
+        
+
+
+        System.out.println("teste gustavo");
+    }
+
+    public static void salvarTeste(String strTexto) throws Exception {
+        String comando = "INSERT INTO teste (teste) VALUES ('" + strTexto + "')";
+        ConexaoMySql executar = new ConexaoMySql();
+        executar.executarComando(comando);
+    }
+    
+    public static void excluirTeste(String strTexto) throws Exception {
+        String comando = "DELETE FROM teste WHERE teste = '" + strTexto + "'";
+        ConexaoMySql executar = new ConexaoMySql();
+        executar.executarComando(comando);
+    }    
+}
